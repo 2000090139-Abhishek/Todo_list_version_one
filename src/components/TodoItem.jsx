8864b3b203f630e0df43f908a7b17b8842ac9e13
2000090139-Abhishek <ac9807623571@gmail.com>
2024-06-25 23:23:32 +0530

    #commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,23 +1,37 @@
 function TodoItem(){
   let todoName="Buy Milk";
   let todoDate="4/10/2023";
+  let todoList = ["todoName","todoDate"];
 
   return(
   
-    <div class="container ">
+    // <div class="container ">
+    //     <div class="row Ab-row">
+    //       <div class="col-6">
+    //         {todoName}
+    //       </div>
+    //       <div class="col">
+    //       {todoDate}
+    //       </div>
+    //       <div class="col">
+    //       <button type="button" class="btn btn-danger Ab-button">
+    //       Delete</button>
+    //       </div>
+    //     </div>
+    // </div>
+    <>
+      <div class="container ">
         <div class="row Ab-row">
           <div class="col-6">
-            {todoName}
-          </div>
-          <div class="col">
-          {todoDate}
-          </div>
-          <div class="col">
-          <button type="button" class="btn btn-danger Ab-button">
-          Delete</button>
+            {todoList.map(item) => (
+              <ul key={(item)}>
+                {item}
+              </ul>
+            ))}
           </div>
         </div>
       </div>
+    </>
 
   );
 }
